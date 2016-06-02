b3c451e57f07e592027fd9461f9b18fdd7678a4c
Jesse Nayak <jessenayak@Jesses-MacBook.local>
2016-06-02 13:33:20 -0400

    Added more fields mirage nodes endpoint

--- mirage/factories/node.js
+++ mirage/factories/node.js
@@ -1,5 +1,11 @@
 import { Factory } from 'ember-cli-mirage';
 
 export default Factory.extend({
-	title(i){ return `Confernce ${i}` }
+	title(i){ return `Confernce ${i}` },
+	type: 'nodes',
+	description:'this is a description of a project',
+	category: 'project',
+	tags() { return [`tag1`,`tag2`] },
+	public: false,
+	contributors: 'bill_wilson'
 });
