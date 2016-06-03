d2e3dea1d75d5ee2f9a7da28884b780f7cbb64a2
Jesse Nayak <jessenayak@Jesses-MacBook.local>
2016-06-03 18:09:45 -0400

    login template

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,13 +1,9 @@
 import Ember from 'ember';
 
-const { RSVP, $ } = Ember;
+const { RSVP} = Ember;
 
 export default Ember.Route.extend({
 	model() {
-		return RSVP.hash({
-	      nodes: $.getJSON('/v2/nodes'),
-	      node2: $.getJSON('/v2/nodes/2'),
-	      node11: $.getJSON('/v2/nodes/11'),
-    	}); 
+		return this.store.findAll('node');
 	}
 });
--- app/transforms/array.js
+++ app/transforms/array.js
@@ -1,17 +1,16 @@
 import Transform from 'ember-data/transform';
+import Ember from 'ember';
 
 export default Transform.extend({
   deserialize(serialized) {
-    return (Ember.typeOf(serialized) == 'array')
-    		? serialized
-    		: [];
+    return (Ember.typeOf(serialized) === 'array') ? serialized : [];
   },
 
   serialize(deserialized) {
   	var type = Ember.typeOf(deserialized);
-  	if(type == 'array'){
+  	if(type === 'array'){
   		return deserialized;
-  	} else if(type == 'string'){
+  	} else if(type === 'string'){
   		return deserialized.split(',').map(function(item){
   			return Ember.$.trim(item);
   		});
