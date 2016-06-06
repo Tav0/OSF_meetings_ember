13d06d99e41c3316b6a7ffc9e96d10242fe8dc55
Tav0 <lv23@vt.edu>
2016-06-06 11:23:34 -0400

    update mirage

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model(function() {
+    model() {
         return [
             { text: "Create Ember app" },
             { text: "Read Ember Igniter" },
