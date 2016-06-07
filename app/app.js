8eb740249af11c99bd81d686e88935b37bc05432
Matt Vitting <vittim@rpi.edu>
2016-06-07 15:57:02 -0400

    Removes mention of DS allusion from ember-data

--- app/app.js
+++ app/app.js
@@ -2,7 +2,6 @@ import Ember from 'ember';
 import Resolver from './resolver';
 import loadInitializers from 'ember-load-initializers';
 import config from './config/environment';
-import DS from 'ember-data';
 
 let App;
 
