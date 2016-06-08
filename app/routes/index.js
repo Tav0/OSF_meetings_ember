93a4a2e5dee9accb5f5570c7b64e49e8ac2e58c4
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-06-08 15:22:23 -0400

    redesigning front landing page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    model() {
-        return this.store.findAll('meeting');
-    }
+  model() {
+    return this.store.findAll('meeting');
+  }
 });
