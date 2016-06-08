1cfe8bf75996199c11a5a721b37592b92fe251c6
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-06-08 17:02:37 -0400

    added scroll animation

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,5 +3,24 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return this.store.findAll('meeting');
+  },
+  actions:
+  {
+    scrollit(){
+
+      Ember.$('#top').hide(2000, function() {
+        Ember.$('#rule').css({position: "fixed", top: "100px"});
+        Ember.$('#createButton').css({position: "fixed", top: "123px"});
+        Ember.$('#meetings').addClass("spacer");
+        Ember.$('#browse').css({position: "fixed"});
+        Ember.$('#meetings').addClass("col-md-offset-3");
+
+      });
+
+
+
+
+      //Ember.$("html, body").animate({ scrollTop: 0 }, "slow");
+    }
   }
 });
