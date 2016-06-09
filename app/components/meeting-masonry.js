072a4433b6be8836fb191d8e627834e72e4dc7f2
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-06-09 16:02:41 -0400

    made front page tiles less imposing and added scrolling description functionality

--- app/components/meeting-masonry.js
+++ app/components/meeting-masonry.js
@@ -1,6 +1,33 @@
 import Ember from 'ember';
 
+
+
 export default Ember.Component.extend({
     title: '',
-    description: ''
+    description: '',
+
+    didInsertElement: function(){
+        var amount = '';
+
+        function scroll()
+        {
+
+          Ember.$('.tile').animate({
+            scrollTop: 15
+          }, 100, 'linear',function() {
+            if (amount !== '') {
+              console.log('MHMM OKAY YEAH');
+              scroll();
+            }
+          });
+        }
+
+
+        Ember.$('.tile').hover(function() {
+          amount += 10;
+          scroll();
+        }, function() {
+          amount = '';
+        });
+      }
 });
