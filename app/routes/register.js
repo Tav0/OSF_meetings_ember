ffda73ff92f7035338b6700a90a0045b8aa27907
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-06-13 16:20:07 -0400

    changing layout of registration form and creating cancel button

--- app/routes/register.js
+++ app/routes/register.js
@@ -1,16 +1,23 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	model() {
-		return this.store.createRecord('meeting');
-	},
-	actions: {
-		back() {
-			this.transitionTo('index').then(function(newRoute) {
-				newRoute.controller.set('clickedCreate', false);
-				newRoute.controller.set('titleError', false);
-				newRoute.controller.set('visited', true);
-			});
-		}
-	}
+  model() {
+    return this.store.createRecord('meeting');
+  },
+  actions: {
+    create(newMeeting){
+      var router = this;
+      newMeeting.save().then(function(){
+        router.transitionTo('conference', newMeeting.id);
+      });
+    },
+    back(newMeeting) {
+      var router = this;
+      newMeeting.destroyRecord().then(function(){
+        router.transitionTo('index').then(function(newRoute) {
+          newRoute.controller.set('visited', true);
+        });
+      });
+    }
+  }
 });
