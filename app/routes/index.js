84ef7547325692b75d014bf1a5f43f8188ba983c
Leandro Alberto-Dominguez <la5aj@virginia.edu>
2016-06-14 16:43:01 -0400

    adding navbar buttons for view selection

--- app/routes/index.js
+++ app/routes/index.js
@@ -9,6 +9,8 @@ export default Ember.Route.extend({
   actions: {
     scrollit(){
       Ember.$('#top').hide(2000, function() {
+        Ember.$('#bottom').css({"margin-top": "80px"});
+        Ember.$('#create').addClass("navbar-fixed-top");
       });
     }
   }
