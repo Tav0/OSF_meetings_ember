81d97141e9f7fea03f5fbd45249b1bdec8a7447a
Jesse Nayak <jdn4ae@virginia.edu>
2016-06-14 19:51:40 -0400

    Extends instead of overrides ember-osf

--- app/models/node.js
+++ app/models/node.js
@@ -1,10 +1,6 @@
-import Model from 'ember-data/model';
-import attr from 'ember-data/attr';
 
-export default Model.extend({
-	title: attr('string'), 
-	contributors: attr('string'), 
-	description: attr('string'), 
-	keywords: attr('array'),
-	category: attr('string'), // this is field is required for api v2 (should be project)
+import OsfModel from 'ember-osf/models/node';
+
+
+export default OsfModel.extend({
 });
