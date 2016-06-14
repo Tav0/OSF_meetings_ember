ff39ac97303b81620ac815462f9a874a27d512f6
Jesse Nayak <jdn4ae@virginia.edu>
2016-06-14 18:35:09 -0400

    Adds trailing slashes to requests

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -3,4 +3,8 @@ import JSONAPIAdapter from 'ember-data/adapters/json-api';
 export default JSONAPIAdapter.extend({
     namespace: 'v2',
     host: 'https://staging-api.osf.io',
+    buildURL: function(type, id, record) {
+    	//call the default buildURL and then append a slash
+    	return this._super(type, id, record) + '/';
+  	}
 });
