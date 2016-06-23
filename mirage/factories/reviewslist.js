bbc6fb1d6c54d2a7abb185d214b09adc47a0bc08
Sherif Abdelhamid <sherief@vt.edu>
2016-06-23 16:22:49 -0400

    styling

--- mirage/factories/reviewslist.js
+++ mirage/factories/reviewslist.js
@@ -2,7 +2,7 @@
 import { Factory, faker } from 'ember-cli-mirage';
 
 export default Factory.extend({
-  title: faker.list.cycle('Analyzing patients records','Repeatability and Benefaction in Computer Systems Research','Social Network Analysis using Genetic Algorithms','Using technology in classrooms'),
+  title: faker.list.cycle('Analyzing patients records','Repeatability and Benefaction in Computer Systems Research','Social Network Analysis using Genetic Algorithms','Reproducible Research for Scientific Computing'),
   conference: faker.list.cycle('ASONAM 2016','ASEE 2015','ICCS 2016','ICCS 2016'),
   reviewdeadline:faker.list.cycle('5/5/2016','2/27/2015','6/30/2016','6/30/2016'),
   reviewer: faker.list.cycle('Tom Adams','Chris David','Rana Jumail','Dan Lewis'),
