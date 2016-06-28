56f1cb9369fa2d48653803aaece79fa05ceb2a15
TomHeatwole <tgh8xn@virginia.edu>
2016-06-28 13:52:38 -0400

    Auto-scroll component updated to support scroll with new styles

--- app/components/auto-scroll.js
+++ app/components/auto-scroll.js
@@ -1,8 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
 	didRender: function() {
-		Ember.$("#top").hide();	
+        Ember.$('#indexTop').hide(0, function() {
+          Ember.$('#indexBottom').css({"margin-top": "80px"});
+          Ember.$('#tableContainer').css({"margin-top": "80px"});
+          Ember.$('#create').addClass("navbar-fixed-top");
+        });
 	}
 });
