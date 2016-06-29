c29b8d10af61dedaec6740ca3b1d9f36f201d9cf
Sherif Abdelhamid <sherief@vt.edu>
2016-06-29 17:03:11 -0400

    delete mirage

--- mirage/factories/submission-eval.js
+++
@@ -1,8 +0,0 @@
-import { Factory } from 'ember-cli-mirage';
-
-export default Factory.extend({
-	premise: 'this is the premise', 
-	research: 'research example', 
-	style: 'style example', 
-	comment: 'sample comment'
-});
