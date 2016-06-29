6c31a73710e1bf19251105baab3ad93307f6889d
Tav0 <lv23@vt.edu>
2016-06-29 16:06:53 -0400

    remove mixin and ember-simple-auth. adapter is fine to use

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,5 +1,3 @@
 import Ember from 'ember';
 
-import OsfLoginControllerMixin from 'ember-osf/mixins/osf-login-controller';
-
-export default Ember.Controller.extend(OsfLoginControllerMixin);
+export default Ember.Controller.extend();
--- app/routes/application.js
+++ app/routes/application.js
@@ -4,9 +4,9 @@ export default Ember.Route.extend({
     store: Ember.inject.service(),
     session: Ember.inject.service(),
     model() {
-        if (this.get('session.isAuthenticated')) {
-            return this.get('store').findRecord('user', 'me');
-        }
-        return null;
-    }
+//        if (this.get('session.isAuthenticated')) {
+//            return this.get('store').findRecord('user', 'me');
+//        }
+//        return null;
+//    }
 });
