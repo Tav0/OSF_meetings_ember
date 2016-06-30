aa15768be50ec575c365daabadf976306703b3b4
Jesse Nayak <jdn4ae@virginia.edu>
2016-06-30 10:22:35 -0400

    Changes adapter from meeting to conference

--- app/routes/application.js
+++ app/routes/application.js
@@ -1,12 +1,4 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-    store: Ember.inject.service(),
-    session: Ember.inject.service(),
-    model() {
-//        if (this.get('session.isAuthenticated')) {
-//            return this.get('store').findRecord('user', 'me');
-//        }
-//        return null;
-//    }
 });
