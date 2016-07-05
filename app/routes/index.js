e7cfc10465145b1e7eb3a13017e7e1ec959a0160
Sherif Abdelhamid <sherief@vt.edu>
2016-07-05 13:43:49 -0400

    remove references to conference model by meetings for landing page

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,11 @@
+
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-	store: Ember.inject.service(),
-	session: Ember.inject.service(),
+  store: Ember.inject.service(),
+  session: Ember.inject.service(),
   model() {
-     return this.store.findAll('conference');
+    //  return this.store.findAll('conference');
   },
   activate: function() {
     Ember.$('body').addClass('hide-scroll');
