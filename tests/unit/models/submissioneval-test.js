3dea9aa6652cd74ed5207b2e6f5dad78c36afc7f
Sherif Abdelhamid <sherief@vt.edu>
2016-07-06 16:55:36 -0400

    new model for submission evaluation

--- tests/unit/models/submissioneval-test.js
+++ tests/unit/models/submissioneval-test.js
@@ -1,6 +1,6 @@
 import { moduleForModel, test } from 'ember-qunit';
 
-moduleForModel('submission-eval', 'Unit | Model | submission eval', {
+moduleForModel('submissioneval', 'Unit | Model | submissioneval', {
   // Specify the other units that are required for this test.
   needs: []
 });
